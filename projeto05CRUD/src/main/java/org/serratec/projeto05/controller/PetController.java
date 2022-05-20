c55b246b986a4723b4efe5b1e5467a9461b437e7
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-05-20 11:19:07 -0300

    Projeto para 20-05 CRUD de uma Classe Escolhida

--- projeto05CRUD/src/main/java/org/serratec/projeto05/controller/PetController.java
+++ projeto05CRUD/src/main/java/org/serratec/projeto05/controller/PetController.java
@@ -1,12 +1,18 @@
 package org.serratec.projeto05.controller;
 
 
+import java.util.List;
+
 import org.serratec.projeto05.model.PetShopModel;
 import org.serratec.projeto05.service.PetShopService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +24,41 @@ public class PetController {
 	@Autowired
 	PetShopService petShopService;
 
+	@GetMapping("/listar")
+	public List<PetShopModel> getPetShop(){
+		return petShopService.listarLojas();
+	}
+	
+	@GetMapping("/buscarPorId/{idLoja}")
+	public ResponseEntity<PetShopModel> buscarPorId(@PathVariable Integer idLoja){
+		return ResponseEntity.ok(petShopService.buscarPorId(idLoja));
+	}
+	
 	@PostMapping("/adicionar")
-		public ResponseEntity<Void> adicionar(@RequestBody PetShopModel petShop){
+	public ResponseEntity<Void> adicionar(@RequestBody PetShopModel petShop){
 		petShopService.adicionar(petShop);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
+	
+	//Busca pelo Id da Loja sem possibilidade de setar o ID
+	@PutMapping("/atualizarPorId/{idLoja}")
+	public ResponseEntity<Void> atualizarPorId(@PathVariable Integer idLoja, @RequestBody PetShopModel petShopDaApi){
+		petShopService.atualizarPorId(idLoja, petShopDaApi);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED); 
+	}
+	
+	//Busca pelo Indice da Lista
+	@PutMapping("/atualizar/{posicaoIndiceLista}")
+	public ResponseEntity<Void> atualizar(@PathVariable Integer posicaoIndiceLista, @RequestBody PetShopModel petShop){
+		petShopService.atualizar(posicaoIndiceLista, petShop);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
+	
+	@DeleteMapping("/deletar/{posicaoLista}")
+	public ResponseEntity<Void> deletar(@PathVariable int posicaoLista){
+		petShopService.deletar(posicaoLista);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
+	
 	
 }
--- projeto05CRUD/src/main/java/org/serratec/projeto05/service/PetShopService.java
+++ projeto05CRUD/src/main/java/org/serratec/projeto05/service/PetShopService.java
@@ -11,10 +11,58 @@ public class PetShopService {
 
 	List<PetShopModel> listaPetShop = new ArrayList<>();
 
+	// Get
+	public List<PetShopModel> listarLojas() {
+		return this.listaPetShop;
+	}
+
+	// Get by ID
+	public PetShopModel buscarPorId(Integer idLoja) {
+		PetShopModel petShopLoja = new PetShopModel();
+		for (PetShopModel petShopLista : listaPetShop) {
+			if (petShopLista.getId().equals(idLoja)) {
+				petShopLoja = petShopLista;
+			}
+		}
+		return petShopLoja;
+	}
+
+	// Post para popular listaPetShop
 	public void adicionar(PetShopModel petShop) {
 		listaPetShop.add(petShop);
 	}
 
+	// Put para atualizar um json pelo ID dele sem possibilidade de alterar seu ID
+	public void atualizarPorId(Integer idLoja, PetShopModel petShopDaApi) {
+		PetShopModel petShopLojaAchada = new PetShopModel();
+		for (PetShopModel petShopLista : listaPetShop) {
+			if (petShopLista.getId().equals(idLoja)) {
+				petShopLojaAchada = petShopLista;
+				
+				petShopLojaAchada.setNome(petShopDaApi.getNome());
+				petShopLojaAchada.setSite(petShopDaApi.getSite());
+
+			}
+		}
+	}
+
+	// Put para atualizar um objeto da Lista por usa posição na Lista podendo alterar inclusive o ID
+	public void atualizar(Integer posicaoLista, PetShopModel petShopDaApi) {
+		PetShopModel petShopDaListaCriada = new PetShopModel();
+		petShopDaListaCriada = listaPetShop.get(posicaoLista);
+
+		petShopDaListaCriada.setId(petShopDaApi.getId());
+		petShopDaListaCriada.setNome(petShopDaApi.getNome());
+		petShopDaListaCriada.setSite(petShopDaApi.getSite());
+
+	}
+	
+	//Delete para deletar o objeto
+	public void deletar(int posicaoLista) {
+		listaPetShop.remove(posicaoLista);
+		
+	}
+	
 	
 
 }
